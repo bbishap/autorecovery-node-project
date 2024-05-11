60302e0f84b0f79f7ed3311c727d058bd67ecc24
Bishap Bhusal <bishapbhusal@MacBook-Pro.local>
2024-05-11 15:54:53 +0545

    added docker compose file

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.post("/increaseMemory", (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server listening on port ${port}`);
 });
 
